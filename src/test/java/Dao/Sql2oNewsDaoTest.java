d1dcb4d121e67a51cebe566083d3b1a6f5fa7776
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-19 18:58:04 +0300

    Add Dao file and all the necessary test files

---
+++ src/test/java/Dao/Sql2oNewsDaoTest.java
@@ -0,0 +1,13 @@
+package Dao;
+
+import junit.framework.TestCase;
+
+public class Sql2oNewsDaoTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
---
+++ src/test/java/Dao/Sql2oUserDaoTest.java
@@ -0,0 +1,13 @@
+package Dao;
+
+import junit.framework.TestCase;
+
+public class Sql2oUserDaoTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
