0664291ebc68d7f821fbb1157e426ac8c58193ef
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-20 15:03:51 +0300

    Add  the CRUD methods in UserDao

--- src/main/java/Dao/UserDao.java
+++ src/main/java/Dao/UserDao.java
@@ -1,4 +1,20 @@
 package Dao;
 
+import model.User;
+
+import java.util.List;
+
 public interface UserDao {
+    //CREATE
+    void add(User user);
+
+    //READ
+    List<User> getAll();
+    User findById(int id);
+
+    //UPDATE
+    void update(int id,String name,String position,String role,int departmentId);
+
+    //DELETE
+    void clearAll();
 }
