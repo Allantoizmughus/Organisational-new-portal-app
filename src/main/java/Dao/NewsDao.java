f5b0e2d90c8ef3d8e3b318a7996da47fde5b81a0
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-20 18:09:32 +0300

    Create CRUD methods in NewsDao interface

--- src/main/java/Dao/NewsDao.java
+++ src/main/java/Dao/NewsDao.java
@@ -1,4 +1,20 @@
 package Dao;
 
+import model.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //CREATE
+    void add(News news);
+
+    //READ
+    List<News> getAll();
+    News findById(int id);
+
+    //UPDATE
+    void update(News news,int id,String content,int userId);
+
+    //DELETE
+    void clearAll();
 }
--- src/main/java/Dao/Sql2oNewsDao.java
+++ src/main/java/Dao/Sql2oNewsDao.java
@@ -1,4 +1,39 @@
 package Dao;
 
-public class Sql2oNewsDao {
+import model.News;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oNewsDao implements NewsDao {
+    private final Sql2o sql2o;
+
+    public Sql2oNewsDao(Sql2o sql2o){
+        this.sql2o=sql2o;
+    }
+
+    @Override
+    public void add(News news) {
+        String sql="INSERT INTO news()";
+    }
+
+    @Override
+    public List<News> getAll() {
+        return null;
+    }
+
+    @Override
+    public News findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(News news, int id, String content, int userId) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
