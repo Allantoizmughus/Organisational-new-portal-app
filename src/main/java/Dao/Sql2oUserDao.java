55188f7be6a26143665812bf0f12f321f90469ab
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-20 15:46:41 +0300

    implement add method in the sql2oUserDao class

--- src/main/java/Dao/Sql2oUserDao.java
+++ src/main/java/Dao/Sql2oUserDao.java
@@ -1,4 +1,47 @@
 package Dao;
 
-public class Sql2oUserDao {
+import model.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oUserDao implements UserDao {
+    private Sql2o sql2o;
+
+    public Sql2oUserDao(Sql2o sql2o){
+        this.sql2o=sql2o;
+    }
+
+    @Override
+    public void add(User user) {
+        String sql="INSERT INTO users(id,name,position,role,departmentId) VALUES(:id, :name, :position,:role, :departmentId)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql,true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String position, String role, int departmentId) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
