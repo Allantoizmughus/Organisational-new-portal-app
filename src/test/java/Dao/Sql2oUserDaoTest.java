a54cc97bc47d5a170fac802802a2dd9371655241
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-21 11:10:34 +0300

    Add the setup and tearDown methods in the Sql2oUserDaoTest class

--- src/test/java/Dao/Sql2oUserDaoTest.java
+++ src/test/java/Dao/Sql2oUserDaoTest.java
@@ -1,13 +1,26 @@
 package Dao;
 
 import junit.framework.TestCase;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-public class Sql2oUserDaoTest extends TestCase {
+public class Sql2oUserDaoTest {
+
+    private static Connection conn;
+    private static Sql2oUserDao usersDao;
+    private static Sql2oDepartmentDao departmentDao;
+    private static Sql2oNewsDao newsDao;
 
     public void setUp() throws Exception {
-        super.setUp();
+        String connectionString = "jdbc:postgresql://localhost:5432/news_api_test";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        usersDao = new Sql2oUserDao(sql2o);
+        conn = sql2o.open();
     }
 
     public void tearDown() throws Exception {
+        conn.close();
     }
 }
