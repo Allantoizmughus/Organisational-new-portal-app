170727aa7b809ce64f3b3a4dcf4d76e64927695a
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-23 02:31:56 +0300

    Add the post method for the departmentUser and departmentNews

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,7 +28,7 @@ public class App {
         userDao = new Sql2oUserDao(sql2o);
         conn = sql2o.open();
 
-        post("/User/new", "application/json", (req,res)->{
+        post("/user/new", "application/json", (req,res)->{
             User user =gson.fromJson(req.body(), User.class);
 
             userDao.add(user);
@@ -37,7 +37,7 @@ public class App {
             return gson.toJson(user);
         });
 
-        post("/News/new", "application/json",(req,res)->{
+        post("/news/new", "application/json",(req,res)->{
             News news=gson.fromJson(req.body(), News.class);
 
             newsDao.add(news);
@@ -46,7 +46,7 @@ public class App {
             return gson.toJson(news);
 
         });
-        post("Department/new","application/json", (req,res)->{
+        post("departments/new","application/json", (req,res)->{
             Department department= gson.fromJson(req.body(), Department.class);
 
             departmentDao.add(department);
@@ -54,6 +54,38 @@ public class App {
             res.type("application/json");
             res.redirect("/department");
             return null;
+        });
+
+        post("/departments/:departmentId/news/:newsId","application/json",(req,res)->{
+            int departmentId = Integer.parseInt(req.params("departmentId"));
+            int newsId = Integer.parseInt(req.params("newsId"));
+            Department department = departmentDao.findById(departmentId);
+            News news = newsDao.findById(newsId);
+
+            if (department != null && news != null) {
+                //both exist and can be associated
+                newsDao.addDepartmentNews(news, department);
+                res.status(201);
+                return gson.toJson(String.format("Department '%s' and News '%s' have been associated", news.getContent(), department.getName()));
+            } else {
+                return null;
+            }
+        });
+
+        post("/departments/:departmentId/users/:userId","application/json",(req,res)->{
+            int departmentId = Integer.parseInt(req.params("departmentId"));
+            int userId = Integer.parseInt(req.params("userId"));
+            Department department = departmentDao.findById(departmentId);
+            User user = userDao.findById(userId);
+
+            if (department != null && user != null) {
+                //both exist and can be associated
+                userDao.addDepartmentUser(user, department);
+                res.status(201);
+                return gson.toJson(String.format("Department '%s' and User '%s' have been associated", user.getName(), department.getName()));
+            } else {
+                return null;
+            }
         });
     }
 }
