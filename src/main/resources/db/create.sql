0504ce0a9ecdeb6b139982186ab1b60a34b815ad
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-24 12:24:35 +0300

    Add new changes to postgres

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -2,14 +2,14 @@ SET MODE PostgreSQL;
 
 CREATE DATABASE news_portal;
 
-CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY,name VARCHAR,position VARCHAR,role VARCHAR,departmentId INTEGER);
+CREATE TABLE users(id serial PRIMARY KEY,name VARCHAR,position VARCHAR,role VARCHAR,departmentId INTEGER);
 
-CREATE TABLE IF NOT EXISTS news(id SERIAL PRIMARY KEY,content VARCHAR,userId INTEGER,departmentId INTEGER);
+CREATE TABLE news(id serial PRIMARY KEY,content VARCHAR,userId INTEGER,departmentId INTEGER);
 
-CREATE IF NOT EXISTS departments(id SERIAL PRIMARY KEY,name VARCHAR,description VARCHAR);
+CREATE TABLE  departments(id serial PRIMARY KEY,name VARCHAR,description VARCHAR);
 
-CREATE IF NOT EXISTS department_news(id SERIAL PRIMARY KEY,departmentId INTEGER, newsId INTEGER);
+CREATE TABLE  department_news(id serial PRIMARY KEY,departmentId INTEGER, newsId INTEGER);
 
-CREATE IF NOT EXISTS department_users(id SERIAL PRIMARY KEY,departmentId INTEGER,userId INTEGER);
+CREATE TABLE  department_users(id serial PRIMARY KEY,departmentId INTEGER,userId INTEGER);
 
 CREATE DATABASE news_portal_test WITH TEMPLATE news_portal;
